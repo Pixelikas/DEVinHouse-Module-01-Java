4fe9e5f41ba1c11489cd69031ec65d1fe15790d2
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2024-08-07 22:37:58 -0300

    Add files via upload

---
+++ Selection Structures If-Else/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        byte personAge = 19;
+
+        if(personAge >= 18){
+
+            System.out.print("You are qualified to drive! =D");
+
+        }else{
+
+            System.out.print("You aren't qualified to drive! =~");
+
+        }
+    }
+}
